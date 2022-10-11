b8dc31e381a02f0174654feb527f4a90831660ef
agutrod <agutrod1910@iesmartinezm.es>
2022-10-11 12:49:48 +0200

    aaaaaaa2

--- Ejercicio 23/script.js
+++ Ejercicio 23/script.js
@@ -4,7 +4,12 @@
 const contarOcurrencias = (cadena) => {
 
   let reversed = cadena.toLowerCase().split("").reverse().join();
-  reversed = reversed.replace(/[\W]/g,"")
+  reversed = reversed.replace(/[\W]/g,"");
+  reversed = reversed.replaceAll(/á/g, "a");
+  reversed = reversed.replaceAll(/é/g, "b");
+  reversed = reversed.replaceAll(/í/g, "c");
+  reversed = reversed.replaceAll(/ó/g, "d");
+  reversed = reversed.replaceAll(/ú/g, "e");
 
   if (cadena.toLowerCase() == reversed) {
     document.getElementById("resultado").innerHTML =
@@ -12,6 +17,8 @@ const contarOcurrencias = (cadena) => {
 
     return true;
   }else{
+    document.getElementById("resultado").innerHTML =
+      "";
     return false;
   }
 };
