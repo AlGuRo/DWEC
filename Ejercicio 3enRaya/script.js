2edc8be21089584b635a9ef2def5a5663e4f751f
tostadita <40300692+Tostadita@users.noreply.github.com>
2022-10-19 23:51:27 +0200

    tres en raya

---
+++ Ejercicio 3enRaya/script.js
@@ -0,0 +1,82 @@
+"use strict";
+let regex = /\([1-3],[1-3]\)/;
+let casillasDisponibles = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const tablero = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+let win = [
+  ["0,0", "0,1", "0,2"], //Fila 0
+  ["1,0", "1,1", "1,2"], //Fila 1 -
+  ["2,0", "2,1", "2,2"], //Fila 2 _
+  ["0,0", "1,1", "2,2"], //Diagonal 0 \
+  ["0,2", "1,1", "2,0"], //Diagonal 1 /
+  ["0,0", "1,0", "2,0"], //Vertical 0 |--
+  ["0,1", "1,1", "2,1"], //Vertical 1 -|-
+  ["0,2", "1,2", "2,2"], //Vertical 2 --|
+];
+
+const tresEnRaya = () => {
+  let haGanado = false;
+  let turno = 0;
+
+  do {
+    haGanado = jugar(turno % 2 === 0 ? "X" : "O");
+
+    turno++;
+  } while (haGanado !== false);
+
+  let jugador = turno % 2 === 0 ? "X" : "O";
+  console.table(tablero);
+  if (haGanado) {
+    return "Ha ganado jugador " + jugador;
+  } else {
+    return "Empate";
+  }
+};
+
+const jugar = (turno) => {
+  let jugada;
+  do {
+    jugada = prompt("Turno jugador " + turno + " , elige casilla (X,Y): ");
+
+    if (jugada.match(regex)) {
+      let row = jugada.at(1);
+      let col = jugada.at(3);
+
+      if (tablero[row - 1][col - 1] === "") {
+        tablero[row - 1][col - 1] = turno;
+
+        if (checkWin(turno)) {
+          return true;
+        }
+      } else {
+        console.log("Posición ocupada");
+      }
+    } else {
+      console.log("Coordenadas invalidas");
+    }
+  } while (!jugada.match(regex));
+};
+
+const checkWin = (turno) => {
+  let haGanado = false;
+  win.map((combinacion) => {
+    combinacion.map((coord) => {
+      let row = coord.at(0);
+      let col = coord.at(2);
+
+      if (tablero[row][col] === turno) {
+        haGanado = true;
+      } else {
+        haGanado = false;
+      }
+    });
+    if (haGanado) {
+      return true;
+    }
+  });
+};
+
+document.getElementById("resultado").innerHTML = tresEnRaya();
