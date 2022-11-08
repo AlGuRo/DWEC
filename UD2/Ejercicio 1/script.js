f77ebbf5b777a3ef3e0c40db05b5fe8780198cc6
agutrod <agutrod1910@iesmartinezm.es>
2022-11-08 11:26:08 +0100

    correccion

--- UD2/Ejercicio 1/script.js
+++ UD2/Ejercicio 1/script.js
@@ -1,12 +1,13 @@
 "use strict"
 
-
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
     let ul = document.querySelector("ul");
     let num = Math.random();
     const li = document.createElement("li");
+    li.style.color = "rgb("+ Math.random()* 256+"," + Math.random()* 256 +","+ Math.random()* 256 +")"; 
+    
     li.textContent=num;
     ul.insertAdjacentElement("afterbegin", li); 
 
