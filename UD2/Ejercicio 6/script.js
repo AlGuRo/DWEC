d4fb0037b1070f498cd0579a53eb837ff8f56343
tostadita <40300692+Tostadita@users.noreply.github.com>
2022-11-17 02:14:04 +0100

    prep

--- UD2/Ejercicio 6/script.js
+++ UD2/Ejercicio 6/script.js
@@ -7,7 +7,3 @@ coord.addEventListener("mousemove", (onmousemove = (result) => {
     coord.innerHTML = "X:" + result.clientX + " Y:" + result.clientY;
   })
 );
-
-let letras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P",
-                  "D", "X", "B", "N", "J", "Z", "S", "Q", "V",
-                  "H", "L", "C", "K", "E"];
